7f2568f7cc7a90d226a9d0bee54d4dc5622cfbf0
piotrkoson <piotr@koson.pl>
2019-09-17 22:06:34 +0200

    Fix webpack config

--- microfrontend1/webpack.common.js
+++ microfrontend1/webpack.common.js
@@ -2,38 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, './angular-app.ts'),
-    resolve: {
-        extensions: ['.js']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'angular-app.html',
-            template: './src/angular-app.html',
-            inject: false
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.resolve(__dirname, './index.html'),
-            inject: false
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true
-                    }
-                }
-            },
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-            }
-        ]
-    }
+  entry: { main: path.resolve(__dirname, './angular-app.ts') },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
+  }
 };
--- microfrontend1/webpack.dev.js
+++ microfrontend1/webpack.dev.js
@@ -4,23 +4,24 @@ const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
-    output: {
-        filename: 'test.js',
-    },
-    devServer: {
-        port: 4300,
-    },
-    devtool: 'inline-source-map',
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         filename: 'angular-app.html',
-    //         template: './src/angular-app.html',
-    //         inject: false
-    //     }),
-    //     new HtmlWebpackPlugin({
-    //         filename: 'index.html',
-    //         template: path.resolve(__dirname, './index.html'),
-    //         inject: false
-    //     })
-    // ]
+  output: {
+    filename: '[name].js'
+  },
+  devServer: {
+    port: 4300
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['main'],
+      filename: 'angular-app.html',
+      template: path.resolve(__dirname, 'src/angular-app.html'),
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/application.html'),
+      inject: false
+    })
+  ]
 });
